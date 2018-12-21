849800af00897c54edee8b5917c8d9c53507ad4c
andee777 <andee.k@hotmail.com>
2018-12-20 22:36:58 -0500

    s

--- sketch.js
+++ sketch.js
@@ -1,8 +1,54 @@
+var current = new Particle();
+
 function setup() {
   createCanvas(720, 400);
+  snowflake = new ArrayList<Particle>();
 
 }
 
 function draw() {
-  background(51);
+  translate(width/2, height/2);
+  background(0);
+  current.update();
+  for(var i =0; i < 6; i++) {
+    current.show();
+    for (Particle p : snowflake) {
+      p.show;
+    }
+  }
+
+}
+
+class Particle {
+  PVector pos;
+  var r;
+  constructor(a, b) {
+    pos.x = a;
+    pos.y = b;
+    r = 3;
+  }
+
+  function update() {
+    pos.x -= 1;
+    pos.y += random(-1, 1);
+
+    float angle = pos.heading();
+
+  }
+
+  function show() {
+    fill(255);
+    stroke(255);
+    ellipse(pos.x, pos.y, r*2, r*2);
+  }
+
+  function intersects() {
+
+  }
+
+  function finished() {
+    return (x < 0);
+  }
+
+
 }
