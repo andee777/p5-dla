31dee8eaa8a5a5219cccc8d30cd94710cfc53dda
andee777 <andee.k@hotmail.com>
2018-12-20 22:20:04 -0500

    s

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(720, 400);
+
+  // Add an initial set of boids into the system
+  for (var i = 0; i < 10; i++) {
+    boids[i] = new Boid(random(width), random(height));
+  }
+}
+
+function draw() {
+  background(51);
+  // Run all the boids
+  for (var i = 0; i < boids.length; i++) {
+    boids[i].run(boids);
+  }
+}
